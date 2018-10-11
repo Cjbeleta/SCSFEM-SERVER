1336e6caaa277024fde9a9704c33f68b20cc0354
Cjbeleta <chrisjabel11@gmail.com>
2018-10-11 19:58:29 +0800

    added add_facility to views, query from api

--- server/urls.py
+++ server/urls.py
@@ -3,9 +3,11 @@ from . import views
 
 urlpatterns = [
     # home route
-    path('home/', views.home),
+    path('', views.landing),
     # facility route
     path('facility/', views.facility),
+    # add facility route
+    path('add_facility', views.add_facility),
     # equipment route
-    path('equipment/', views.equipment)
+    path('equipment/', views.equipment),
 ]
--- server/views.py
+++ server/views.py
@@ -1,14 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
-
-def home(request):
-    return render(request, 'server/home.html')
-
+@csrf_exempt
+def landing(request):
+    return render(request, 'server/landing.html')
+@csrf_exempt
+def dashboard(request):
+    return render(request, 'server/dashboard.html')
+@csrf_exempt
 def facility(request):
-    content = {'facilities' : [{'name': 'Facility A'}, {'name': 'Facility B'}]}
+    facilities = requests.get('http://localhost:8000/api/facility/')
+    print (facilities.json())
+    content = {'facilities' : facilities.json()}
     return render(request, 'server/facility.html', {'content': content})
-
+@csrf_exempt
+def view_facility(request):
+    return render(request, 'server/view_facility.html')
+@csrf_exempt
+def add_facility(request):
+    if request.method == 'POST':
+        data = {'data': [{'name': request.POST['name'], 'status': request.POST['status']}]}
+        requests.post('http://localhost:8000/api/facility/', data)
+        return redirect(facility)
+    elif request.method == 'GET':
+        return render(request, 'server/add_facility.html')
+@csrf_exempt
+def edit_facility(request):
+    return render(request, 'server/edit_facility.html')
+@csrf_exempt
 def equipment(request):
     content = {'equipments': [{'name': 'Equipment A'}, {'name': 'Equipment B'}]}
     return render(request, 'server/equipment.html', {'content': content})
