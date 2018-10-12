8f87a220400bd33d99b3e455b68605fd21eba203
Cjbeleta <chrisjabel11@gmail.com>
2018-10-12 09:11:54 +0800

    modified add_facility and edit_facility

--- server/urls.py
+++ server/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('facility/', views.facility),
     # add facility route
     path('add_facility', views.add_facility),
+    # edit facility route
+    path('edit_facility/<str:pk>', views.edit_facility),
     # equipment route
     path('equipment/', views.equipment),
 ]
--- server/views.py
+++ server/views.py
@@ -21,14 +21,20 @@ def view_facility(request):
 @csrf_exempt
 def add_facility(request):
     if request.method == 'POST':
-        data = {'data': [{'name': request.POST['name'], 'status': request.POST['status']}]}
-        requests.post('http://localhost:8000/api/facility/', data)
+        data = {'name': request.POST['name'], 'status': request.POST['status']}
+        print (data)
+        response = requests.post('http://localhost:8000/api/facility/', data=data)
+        print (response)
         return redirect(facility)
     elif request.method == 'GET':
         return render(request, 'server/add_facility.html')
 @csrf_exempt
-def edit_facility(request):
-    return render(request, 'server/edit_facility.html')
+def edit_facility(request, pk):
+    url = 'http://localhost:8000/api/facility/' + pk + '/'
+    print (url)
+    facility = requests.get(url)
+    print (facility)
+    return render(request, 'server/edit_facility.html', {'facility': facility.json()})
 @csrf_exempt
 def equipment(request):
     content = {'equipments': [{'name': 'Equipment A'}, {'name': 'Equipment B'}]}
